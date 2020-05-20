b7e0616d195b8beea7f55dbcae4f1c43f72921b7
Remzi <kocak0068@gmail.com>
2020-05-20 23:11:25 +0200

    Added HasGravatar Trait

---
+++ src/Traits/HasGravatar.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace RKocak\Gravatar\Traits;
+
+
+use RKocak\Gravatar\Facades\Gravatar;
+use RKocak\Gravatar\Generator;
+
+trait HasGravatar
+{
+
+    /**
+     * @return string
+     */
+    public function getGravatar(): string
+    {
+        return Gravatar::url($this->email);
+    }
+
+    /**
+     * @return Generator
+     */
+    public function getGravatarGenerator(): Generator
+    {
+        return Gravatar::for($this->email);
+    }
+    
+}
